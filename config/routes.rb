50397a04ba421dd1f22d16e60cf1e771aa153aa5
sumika129 <112753628+sumika129@users.noreply.github.com>
2022-12-17 08:50:54 +0000

    update public/session

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  root to: "public/homes#top"
+
   namespace :admin do
     root to: "homes#top"
     resources :genres, only: [:index, :create, :edit, :update]
-    resources :items, only: [:new, :create, :index, :show, :edit, :update
-    ]
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit]
+  end
+
+  namespace :public do
+
+    get 'home/about'=>'homes#about',as:'about'
   end
 
   devise_for :customers,skip: [:passwords], controllers: {
